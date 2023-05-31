f30ead07db359f18a791e059586dd954919dbb53
higinot <94652743+higinot@users.noreply.github.com>
2023-05-31 14:14:02 +0000

    Terminando exercicios de filtragem

---
+++ desafio13.sql
@@ -0,0 +1,7 @@
+-- Active: 1685540807740@@127.0.0.1@3306@northwind
+SELECT supplier_id FROM purchase_orders
+    WHERE supplier_id = 1 OR supplier_id = 2;
+
+
+SELECT id, supplier_id FROM purchase_orders
+    WHERE su;
